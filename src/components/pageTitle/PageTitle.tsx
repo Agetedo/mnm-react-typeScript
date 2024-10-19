1c6069c9d31fc5cd3683455121f09137c0c13156
Agetedo <agetedo@gmail.com>
2024-10-19 19:52:19 +0300

    update <PageTitle />

--- src/components/pageTitle/PageTitle.tsx
+++ src/components/pageTitle/PageTitle.tsx
@@ -3,6 +3,16 @@ import PathConstants from "../../routes/pathConstants";
 import pageTitleIcon from "/page-title-icon.svg";
 import "./PageTitle.scss";
 
+export default function PageTitle({ pageTitleLink }: { pageTitleLink: string }) {
+  return (
+    <PageTitleConteiner className="procces-link">
+      <Link to={PathConstants.PROCESSPAGE} id="pageTitle">
+        <img src={pageTitleIcon} alt="#"/>{pageTitleLink}
+      </Link>
+    </PageTitleConteiner>
+  );
+}
+
 interface PageTitleProps {
   className: string;
   children?: React.ReactNode;
@@ -12,15 +22,5 @@ function PageTitleConteiner({ children, className }: PageTitleProps) {
     <div className={className}>
       <div className="procces-link__conteiner">{children}</div>
     </div>
-  );
-}
-
-export default function PageTitle({ pageTitleLink }: { pageTitleLink: string }) {
-  return (
-    <PageTitleConteiner className="procces-link">
-      <Link to={PathConstants.PROCESSPAGE} id="pageTitle">
-        <img src={pageTitleIcon} alt="#"/>{pageTitleLink}
-      </Link>
-    </PageTitleConteiner>
   );
 }
